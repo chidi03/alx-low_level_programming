3ce14f351b18cdae99cc3f2a073b5012e03bb922
chidi03 <menorra333@gmail.com>
2022-10-21 10:17:02 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers 0 - 9 plus a new line.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar((i % 10) + '0');
+	}
+
+	_putchar('\n');
+}
