c784549537be3a253a90b9be2d3b80cad9bb1221
chidi03 <menorra333@gmail.com>
2022-10-27 18:04:27 -0700

    a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all the words of a string.
+ * @str: the string to be capitalized.
+ * Return: a pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[inedx] >= 'a' && str[index] <= 'z'))
+			index++
+
+		if (str[index - 1] == ' ' || str [index - 1] == '\t'
+	}
+}
