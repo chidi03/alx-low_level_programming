364012d6eadf0222e59f2a5bd149257aaa3b4f3e
chidi03 <menorra333@gmail.com>
2022-11-14 12:31:13 -0800

    Pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * file: 1-pi.h
+ * auth: chidi03
+ * desc: header file that defines a macro named PI as an abbreviation for 
+ *		the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
