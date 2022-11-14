591b44befb1b76d6a523775a38bb2919d1468308
chidi03 <menorra333@gmail.com>
2022-11-14 12:17:14 -0800

    Object-like Macro

--- 0-object_like_macro.h
+++ 0-object_like_macro.h
@@ -1,6 +1,13 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
+/**
+ * file: 0-object_like_macro.h
+ * Auth: chidi03
+ * desc: header file that contains a macro named SIZE as an abbreviation
+ *		for the token 1024
+ */
+
 #define SIZE 1024
 
 #endif
