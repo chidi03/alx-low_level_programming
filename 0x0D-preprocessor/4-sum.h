c80ff36892a633a9f43260a91e6f46277298bf9d
chidi03 <menorra333@gmail.com>
2022-11-14 18:10:24 -0800

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * file: 4-sum.h
+ * auth: chidi03
+ * desc: header file that defines a function-like macro SUM(x, y) that 
+ *		computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
