f1f5243d1e8c4436b2cf103b0e11b8198acb1096
chidi03 <menorra333@gmail.com>
2022-11-14 18:01:25 -0800

    function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
+/*
  * File - 3-function-like-macro.h
  * auth: chidi03
  * desc: header files that defines a function-like macro ABS(x) that computes
