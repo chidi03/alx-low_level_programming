c07a5f7f76f5966337da34d46bb043c1a42dd92a
chidi03 <menorra333@gmail.com>
2022-11-14 18:24:00 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,7 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /*
- * File - 3-function-like-macro.h
+ * file - 3-function-like-macro.h
  * auth: chidi03
  * desc: header files that defines a function-like macro ABS(x) that computes
  *		the absolute value of a number x.
